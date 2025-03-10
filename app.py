b9fd671b14b442dae9c5ee73de024c1414d26373
Ella Gordon <ellaggordon@gmail.com>
2025-03-09 18:25:14 -0700

    implemented calls for service B, C, and D

--- app.py
+++ app.py
@@ -1,35 +1,43 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify
+import requests
+import json
 from pymongo import MongoClient
 
 app = Flask(__name__)
 
-CONNECTION_STRING = 
-# MongoDB connection
+CONNECTION_STRING = "mongodb+srv://ellaggordon:breadpottery123@players.rbqni.mongodb.net/"
+
 client = MongoClient(CONNECTION_STRING)
-db = client["SoftwareEngineeringI"]  # Replace with your database name
-collection = db["players"]  # Replace with your collection name
+db = client["SoftwareEngineeringI"]
+collection = db["players"]
 
 @app.route("/", methods = ["GET"])
 def home():
     return render_template("index.html")
 
 @app.route("/positionSort", methods = ["GET"])
-def positionSort():
+def positionSortDefault():
     return render_template("positionSort.html")
+
+@app.route("/positionSort/<players_json>", methods = ["GET"])
+def positionSort(players_json):
+    players = json.loads(players_json)
+    return render_template("positionSort.html", players=players)
 
 @app.route("/addPlayer", methods = ["GET"])
 def addPlayer():
     return render_template("addPlayer.html")
 
 @app.route("/search", methods = ["GET"])
-def search():
+def search(players=[]):
     return render_template("search.html")
 
 @app.route("/viewAll", methods = ["GET"])
 def viewAll():
-    players = list(collection.find({}, {"_id": 0}))  # Fetch all players
+    response = requests.get("http://127.0.0.1:3000/display-all")
+    players = response.json()
+    # print(players)
     return render_template("viewAll.html", players=players)
-    #return render_template("viewAll.html")
 
 # Add a player to database
 @app.route("/addPlayer", methods=["POST"])
@@ -39,6 +47,7 @@ def add_player():
     position = request.form.get("position")
     goals_scored = int(request.form.get("goals_scored"))
 
+
     new_player = {
         "name": name,
         "age": age,
@@ -46,40 +55,53 @@ def add_player():
         "goals_scored": goals_scored
     }
 
-    collection.insert_one(new_player)
+    print(new_player)
+
+    new_player_json = jsonify(new_player)
+    print(new_player_json)
+    temp = requests.post("http://127.0.0.1:4000/add-player", json = new_player)
 
     return redirect(url_for('addPlayer'))
-
-# Get every player in the database and display them all
-@app.route('/viewAll')
-def show_players():
-    players = list(collection.find({}, {"_id": 0})) 
-    return render_template('viewAll.html', players=players)
 
 @app.route('/search', methods=['POST'])
 def search_player():
     name = request.form.get("name")  # Get the name from the form
+    name = name.replace(" ", "+")
+    print(name)
+    players = requests.get("http://localhost:8080/players/namedplayers?name=" + name)
 
-    # Search for the player in MongoDB (case-insensitive search)
-    player = collection.find_one({"name": {"$regex": f"^{name}$", "$options": "i"}}, {"_id": 0})
+    print(players.json())
 
-    if player:
-        return render_template('search.html', player=player)
-    else:
-        return render_template('search.html', message="Player not found.")
-
-@app.route('/positionSort', methods=['GET', 'POST'])
-def search_by_position():
-    if request.method == 'POST':
-        position = request.form.get("position")
-
-        players = list(collection.find({"position": position}, {"_id": 0})) 
-        if players:
-            return render_template('positionSort.html', players=players, position=position)
-        else:
-            return render_template('positionSort.html', message="No players found in position: {position}")
     
-    return render_template('search_position.html')
+    # return render_template("search.html", players=players)
+    # return redirect(url_for("search", players=players))
+    return "" 
+
+
+
+    # # Search for the player in MongoDB (case-insensitive search)
+    # player = collection.find_one({"name": {"$regex": f"^{name}$", "$options": "i"}}, {"_id": 0})
+
+    # if players:
+    #     return render_template("search.html", players=players)
+    # else:
+    #     return render_template('search.html', message="Player not found.")
+
+@app.route('/positionSort', methods=['POST'])
+def search_by_position():
+
+    position = request.form.get("position")
+
+    response = requests.get("http://localhost:5000/sort-by-position?position=" + position)
+    players = response.json()
+    players_json = json.dumps(players)
+
+    print(players)
+
+    if players:
+        return redirect(url_for("positionSort", players_json=players_json))
+    else:
+        return redirect(url_for('positionSortDefault'))
 
 if __name__ == "__main__":
     app.run(port = 2000, debug = True)
